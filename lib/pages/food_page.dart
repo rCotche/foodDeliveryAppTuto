2182af3bf8ef2bbbf81975e324e41ef0b909e944
rCotche <roshan.cotche@gmail.com>
2024-06-09 22:15:30 +0400

    maj food details

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -12,14 +12,36 @@ class FoodPage extends StatefulWidget {
 class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        //widget.food.imagePath
-        //Pour le moment
-        Image.asset(
-          "lib/images/placeholder.png",
-        ),
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          //food image
+          //widget.food.imagePath
+          //Pour le moment
+          Image.asset(
+            "lib/images/placeholder.png",
+          ),
+
+          //food name
+          Text(widget.food.name),
+
+          //food description
+          Text(widget.food.description),
+
+          //addons
+          ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: widget.food.availableAddon.length,
+              itemBuilder: (context, index) {
+                //get individual addon
+                final Addon anAddon = widget.food.availableAddon[index];
+                return CheckboxListTile(value: false, onChanged: (value) {});
+              }),
+
+          //button add to cart
+        ],
+      ),
     );
   }
 }
