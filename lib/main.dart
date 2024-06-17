04562a7671379a5a2ca27c7db9d2d25bcf04aa45
rCotche <roshan.cotche@gmail.com>
2024-06-17 18:48:55 +0400

    firebase part

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:food_delivery/auth/login_or_register.dart';
+import 'package:food_delivery/services/auth/login_or_register.dart';
+import 'package:food_delivery/firebase_options.dart';
 import 'package:food_delivery/models/restaurant.dart';
 import 'package:food_delivery/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  //
+  WidgetsFlutterBinding.ensureInitialized();
+  //
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   //Ce widget fournira nos classe à tous les widgets descendants qui en ont besoin.
   runApp(MultiProvider(
     providers: [
